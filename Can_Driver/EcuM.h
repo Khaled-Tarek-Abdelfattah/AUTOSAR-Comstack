6da0ff3c75211d50cbd861049c9c157666e5ecb0
khaled Tarek <73825127+Khaled-Tarek-Abdelfattah@users.noreply.github.com>
2023-12-11 15:38:51 +0200

    Delete Can_Driver directory

--- Can_Driver/EcuM.h
+++
@@ -1,8 +0,0 @@
-
-/****************EcuM_WakeupSourceType**************/
-
-#define ECUM_WKSOURCE_POWER                          ((uint32)1U)
-#define ECUM_WKSOURCE_RESET                          ((uint32)2U)
-#define ECUM_WKSOURCE_INTERNAL_RESET                 ((uint32)4U)
-#define ECUM_WKSOURCE_INTERNAL_WDG                   ((uint32)8U)
-#define ECUM_WKSOURCE_EXTERNAL_WDG                   ((uint32)16U)
